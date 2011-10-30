2b0ab208175d57fb3c35e5f31f2527824f862a74
David Grayson <davidegrayson@gmail.com>
2011-10-29 18:05:07 -0700

    Replaced go*.rb with solve.rb.

--- go_hex.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-require_relative 'hexadoku'
-require_relative 'puzzles/elektor/2011-11'
-
-puts $puzzle.solve
---
+++ solve.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+if ARGV.size != 1
+  puts "Usage: solve.rb path_to_puzzle.rb"
+  puts "Example: solve.rb puzzles/elektor/2011-11.rb"
+  puts "The .rb extension is optional."
+  exit 1
+end
+
+require_relative 'sudoku'
+require_relative 'hexadoku'
+require_relative ARGV[0]
+
+puts $puzzle.solve
