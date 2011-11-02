fc1c218d66d19622960095d8d351a095ad4ea9e2
David Grayson <davidegrayson@gmail.com>
2011-11-01 22:17:36 -0700

    I noticed that the tests get half a second faster if I remove the deduced groups from the Hexamurai class, so I added a TODO note to see if those deduced groups slow down the algorithm for a real puzzle (i.e. one that takes an hour to solve).

--- lib/doku/hexamurai.rb
+++ lib/doku/hexamurai.rb
@@ -77,6 +77,7 @@ END
     # These groups are not explicitly stated in the puzzle, but
     # we can use logic to deduce that they are groups (no
     # glyph can appear twice in them).
+    # TODO: see if these deduced groups actually speed up algorithm!
     8.upto(23) do |n|
       define_group row(n, 0, 8) + row(n, 24, 8)
       define_group column(n, 0, 8) + column(n, 24, 8)
