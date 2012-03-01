30103afc5bdb95d9dbad9750245c7ad3533b8e84
David Grayson <davidegrayson@gmail.com>
2012-02-29 19:45:23 -0800

    Made the lines end at the right point for Hexamurai (and similar puzzles).

--- lib/doku/svg.rb
+++ lib/doku/svg.rb
@@ -25,11 +25,11 @@ END
   def to_svg
     require 'builder'
 
-    max_x = squares.collect(&:x).max
-    max_y = squares.collect(&:y).max
+    abs_max_x = squares.collect(&:x).max
+    abs_max_y = squares.collect(&:y).max
 
-    width = (max_x+1)*SquareWidth + Margin*2
-    height = (max_y+1)*SquareWidth + Margin*2
+    width = (abs_max_x+1)*SquareWidth + Margin*2
+    height = (abs_max_y+1)*SquareWidth + Margin*2
 
     xml_string = ""
     builder = Builder::XmlMarkup.new :target=>xml_string, :indent=>2
@@ -43,17 +43,33 @@ END
       end
 
       builder.g :transform=>"translate(#{Margin}, #{Margin})" do
-        (0..(max_x+1)).each do |x|
-          options = {:x1 => x*SquareWidth, :y1 => 0, :x2 => x*SquareWidth, :y2 => (max_y+1)*SquareWidth}
-          if x > 0 && x < max_x+1 && self.class.char_number[x] - self.class.char_number[x-1] > 1
+        (0..(abs_max_x+1)).each do |x|
+          left_x = x > 0 ? x - 1 : nil
+          right_x = x <= abs_max_x ? x : nil
+          xs = [left_x, right_x]
+          ys = squares.select { |s| xs.include?(s.x) }.collect(&:y)
+
+          options = {}
+          options[:x1] = options[:x2] = x*SquareWidth
+          options[:y1] = ys.min*SquareWidth
+          options[:y2] = (ys.max+1)*SquareWidth
+          if left_x && right_x && self.class.char_number[right_x] - self.class.char_number[left_x] > 1
             options[:class] = "heavy"
           end
           builder.line options
         end
 
-        (0..(max_y+1)).each do |y|
-          options = {:x1 => 0, :y1 => y*SquareWidth, :x2 => (max_x+1)*SquareWidth, :y2 => y*SquareWidth}
-          if y > 0 && y < max_x+1 && self.class.line_number[y] - self.class.char_number[y-1] > 1
+        (0..(abs_max_y+1)).each do |y|
+          left_y = y > 0 ? y - 1 : nil
+          right_y = y <= abs_max_y ? y : nil
+          ys = [left_y, right_y]
+          xs = squares.select { |s| ys.include?(s.y) }.collect(&:x)
+
+          options = {}
+          options[:y1] = options[:y2] = y*SquareWidth
+          options[:x1] = xs.min*SquareWidth
+          options[:x2] = (xs.max+1)*SquareWidth
+          if left_y && right_y && self.class.char_number[right_y] - self.class.char_number[left_y] > 1
             options[:class] = "heavy"
           end
           builder.line options
